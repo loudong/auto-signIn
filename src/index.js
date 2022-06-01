af56d3b329ef48f746c9124662e64d15da270f26
墨晔 <moye@moyedeMacBook-Pro.local>
2022-06-01 11:41:01 +0800

    feat:配置抽奖

--- src/index.js
+++ src/index.js
@@ -7,9 +7,18 @@ const app = new koa()
 let num = 0
 const sessionid = '4405867d2840c7a2228fbee2d52f7086'; // SessionID
 const url = 'https://api.juejin.cn/growth_api/v1/check_in?aid=2608&uuid=7091537654956885511&_signature=_02B4Z6wo02901G5SeagAAIDBFpySrGJPZPxuRn0AAHkJbSHP038FJedFXL1m2Pou.-65IeQlelu1PLmAUHMKvaxclCa.KiE3g09.77Fmem9OsNm4ODXc4iJxR300nhcVPtYTcnC2FSpikveE0f'; // Url
+const choujianUrl = 'https://api.juejin.cn/growth_api/v1/lottery/draw?aid=2608&uuid=7091537654956885511&_signature=_02B4Z6wo02901G5SeagAAIDBFpySrGJPZPxuRn0AAHkJbSHP038FJedFXL1m2Pou.-65IeQlelu1PLmAUHMKvaxclCa.KiE3g09.77Fmem9OsNm4ODXc4iJxR300nhcVPtYTcnC2FSpikveE0f'
 const options = {
     url: url,
     method:'post',
+    headers: {
+        'cookie': 'sessionid='+ sessionid,
+    },
+}
+
+const choujiangOptions = {
+    url: choujianUrl,
+    method: 'post',
     headers: {
         'cookie': 'sessionid='+ sessionid,
     },
@@ -23,13 +32,7 @@ const transport = mail.createTransport({
         pass: 'oncazqeehtjrbfea'
     }
 })
-const mailOption = {
-    from: '生活就像一场梦 767416042@qq.com',
-    to: 'moye@yowant.com',
-    subject: '每日打卡',
-    html: '<p>打卡成功</p>',
 
-}
 function request(url, options) {
     return new Promise(function (resolve, reject) {
         _request(url, options, function (error, response, body) {
@@ -42,10 +45,29 @@ async function start (ctx, next) {
     num += 1
     console.log(`第${num}次请求`);
     const res = await request(options);
-    console.log('结果', res.body)
+    const body = JSON.parse(res.body)
+    console.log('签到成功', body)
+    const { err_msg, data } = body || {}
+    const choujiangResult = await request(choujiangOptions)
+    const choujiangResultBody = JSON.parse(choujiangResult.body)
+    console.log('抽奖成功', choujiangResultBody);
+    const { data: choujian_data, err_msg: choujian_msg } = choujiangResultBody
+    const mailOption = {
+        from: '生活就像一场梦 767416042@qq.com',
+        to: 'moye@yowant.com',
+        subject: '每日打卡',
+        html: `<div>
+            <p>打卡结果：${err_msg}</p>
+            <p>获得矿石：${data && data.incr_point || '-'}</p>
+            <p>共计矿石：${data && data.sum_point || '-'}</p>
+            <p>抽奖状态: ${choujian_msg}</p>
+            <p>抽奖结果：${choujian_data && choujian_data.lottery_name} <img width="40px" height="40px" src=${choujian_data.lottery_image} /></p>
+            <p>当前幸运值： ${choujian_data && choujian_data.total_lucky_value}</p>
+        </div>`,
+    }
     transport.sendMail(mailOption, (err, response) => {
-        if(err) { console.log(err) }
-        else { console.log(response)}
+        if(err) { console.log('邮件错误', err) }
+        else { console.log('邮件结果', response)}
     })
 }
 
